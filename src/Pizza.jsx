13bb6485828a4f9cd23d4625de753432c347c6a8
Neelima Akkiraju <neelimaakkiraju2001@gmail.com>
2025-01-10 12:34:38 +0530

    connected to api server

---
+++ src/Pizza.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Pizza = (props)=>{
+    return(
+        <div>
+            
+            <h2>{props.name}</h2>
+            <p>{props.description}</p>
+        </div>
+    )
+}
+
+export default Pizza;
