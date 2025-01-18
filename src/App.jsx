ce4ccafb3b90ad1778e2be254dd6d610542eac9f
Neelima Akkiraju <neelimaakkiraju2001@gmail.com>
2025-01-18 12:09:44 +0530

    imported strict mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import Pizza from "./Pizza";
 import Order from "./Order";
+import { StrictMode } from "react";
 
 const App = () => {
   return (
-    <div>
+    <StrictMode>
+      <div>
       <h1 className="logo">Padre Gino's Pizza – Order Now</h1>
       <Order />
     </div>
+    </StrictMode>
   );
 };
 const container = document.getElementById("root");
